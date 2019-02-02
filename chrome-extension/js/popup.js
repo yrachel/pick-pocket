a2c2de310ac2744eb9329faa59da415294d3a949
jasontyu <jasonyu6154@gmail.com>
2019-02-02 17:24:55 -0500

    get button to run test script

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,25 +1,28 @@
 let toggleState = document.getElementById('toggleState');
 
-chrome.storage.sync.get('onState', function(data) {
-    if (data.onState == 'off'){
+function updateToggleButton(onState) {
+    if (onState == 'off'){
         toggleState.style.backgroundColor = '#abcdef';
     }
     else {
-        toggleState.style.backgroundColor = '#123456';
+        toggleState.style.backgroundColor = '#654321';
     }
-    toggleState.setAttribute('value', data.onState);
+    toggleState.setAttribute('value', onState);
+}
+
+chrome.storage.sync.get('onState', function(data) {
+    updateToggleButton(data.onState);
 });
 
-
 toggleState.onclick = function() {
-    console.log('clicked toggleState');
     chrome.storage.sync.get('onState', function(data) {
-        if (data.onState == 'off') {
-            data.onState = 'on'
-        } else {
-            data.onState = 'off' }
-        if (data.onState == 'on') {
-            chrome.tabs.executeScript({
+        var newState = data.onState=='off' ? 'on' : 'off';
+        chrome.storage.sync.set({onState: newState}, function callback() {
+            console.log("The onState is updated.");
+        });
+        updateToggleButton(data.onState);
+        if (newState == 'on') {
+            chrome.tabs.executeScript(null,{
                 file: 'js/contentTest.js'
             });
         }
