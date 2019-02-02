58c401a3f46911ff3e5142d491f2080b23a48f0c
jasontyu <jasonyu6154@gmail.com>
2019-02-02 17:33:08 -0500

    move serverside files into its own folder

---
+++ serverside/backend.py
@@ -0,0 +1,41 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "pickpocket-0977cb997aac.json"
+from flask import Flask, render_template, redirect, url_for,request, send_from_directory
+from flask import make_response
+from google.cloud import vision
+app=Flask(__name__, static_url_path='')
+
+@app.route('/index.html')
+def send_index():
+    #return send_from_directory('html', 'chrome-extension/test.html')
+    page = open('chrome-extension/test.html', 'r')
+    return page.read()
+
+@app.route('/analyze')
+def send_js():
+    page = open('chrome-extension/analyze.js', 'r')
+    return page.read()
+
+@app.route('/has_pocket', methods=['GET', 'POST'])
+def has_pocket():
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    url = request.form['url']
+    threshold = request.form['threshold']
+
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = url
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    pocketness = [obj.score for obj in labels if obj.description == 'Pocket']
+    if pocketness[0] > threshold:
+        return "true"
+    else:
+        return "false"
+
+if __name__ == "__main__":
+    app.run()
+
+#print(has_pocket('https://cdn.shopify.com/s/files/1/0139/8942/products/womens-casma-two-pocket-scrub-top_navy-4_900x.jpg?v=1549069425', 0.5))
